010debd06d6305d47f15aeed12112ee8249a4f53
SERKAN CAVUS <40465585+SCavus@users.noreply.github.com>
2020-04-22 22:36:40 +0100

    100-no styling

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //You can edit ALL of the code here
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -7,6 +8,33 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const mySearchInput = document.createElement("input");
+  mySearchInput.onchange = function(e){
+    console.log(e.target.value);
+    const filterEpisode = episodeList.filter(element => {
+      if(element.summary.includes(e.target.value) || element.name.includes(e.target.value))
+      {
+        // console.log(element)
+        return element;
+      }
+    })
+ console.log(filterEpisode)
+  }
+  rootElem.appendChild(mySearchInput);
+  episodeList.forEach(element => { 
+    // console.log(element);
+    const myTitle = document.createElement("h3");
+    const container = document.createElement("div");
+    const myImg =  document.createElement("img");
+    const mySummary = document.createElement("p");
+    myTitle.textContent = `${element.name} S${element.season < 10 ? "0":""}${element.season} E${element.number < 10 ? 0:""}${element.number}`;
+    container.appendChild(myTitle);
+     myImg.src = element.image.medium;
+     container.appendChild(myImg);
+      mySummary.innerHTML = element.summary;
+     container.appendChild(mySummary);
+    rootElem.appendChild(container);
+  });
 }
 
 window.onload = setup;
