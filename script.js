dfc84cb13151397a4d639b36ff402365602721d5
SERKAN CAVUS <40465585+SCavus@users.noreply.github.com>
2020-05-03 00:40:55 +0100

    400-needs debugging

--- script.js
+++ script.js
@@ -1,73 +1,110 @@
 //You can edit ALL of the code here
-
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
-function makePageForEpisodes(episodeList) {
-  const homebtn = document.createElement('button')
-  homebtn.textContent = 'Home'
-  homebtn.className = 'home'
-  const search = document.querySelector("#search");
-  const searchInput = document.createElement("input");
-  const episodeNum = document.createElement("p");
-  search.appendChild(homebtn)
-  displayPage(episodeList);
-  const select = document.createElement('select')
-  select.id = 'selection'
-  search.appendChild(select)
-  searchInput.type = "text";
-  searchInput.placeholder = "Search";
-  search.appendChild(searchInput);
-  search.appendChild(episodeNum);
-  episodeNum.style.marginLeft = "30px";
-  episodeNum.textContent = `${episodeList.length} episode(s)`;
-  searchInput.addEventListener("keyup", function(e) {
-    e.preventDefault();
-    let term = searchInput.value.toLowerCase();
-    let filterEpisode = episodeList.filter(
-      element =>
-        element.name.toLowerCase().includes(term) ||
-        element.summary.toLowerCase().includes(term)
-    );
-    displayPage(filterEpisode);
-    episodeNum.textContent = `${filterEpisode.length} episode(s)`;
-  });
+  let allShow = getAllShows();
+  makePageForEpisodes(allShow);
 
-  function displayPage(list) {
-    const container = document.getElementById("container");
-    container.innerHTML = "";
-    list.forEach(element => {
-      const episode = document.createElement("div");
-      const title = document.createElement("h3")
-      const img = document.createElement("img")
-      const summary = document.createElement("p")
-      episode.className = 'episode'
-      title.className = 'title'
-      img.className = 'image'
-      summary.className = 'summary'
-      title.innerHTML = `${element.name} - S${element.season < 10 ? 0 : ""}${element.season}E${element.number < 10 ? 0 : ""}${element.number}`
-      episode.appendChild(title)
-      img.src = element.image.medium
-      episode.appendChild(img)
-      summary.innerHTML = element.summary
-      episode.appendChild(summary)
-      container.appendChild(episode);
+  function makePageForEpisodes(episodeList) {
+    const homebtn = document.createElement("button");
+    homebtn.textContent = "Home";
+    homebtn.className = "home";
+    const search = document.querySelector("#search");
+    const searchInput = document.createElement("input");
+    const episodeNum = document.createElement("p");
+    search.appendChild(homebtn);
+    displayPage(episodeList);
+    let selectShow = document.createElement("select");
+    selectShow.className = "selectshow";
+    search.appendChild(selectShow);
+    let selectEpisode = document.createElement("select");
+    selectEpisode.className = "select";
+    search.appendChild(selectEpisode);
+    searchInput.type = "text";
+    searchInput.placeholder = "Search";
+    search.appendChild(searchInput);
+    search.appendChild(episodeNum);
+    episodeNum.style.marginLeft = "30px";
+    episodeNum.textContent = `${episodeList.length} episode(s)`;
+    searchInput.addEventListener("keyup", function (e) {
+      e.preventDefault();
+      let term = searchInput.value.toLowerCase();
+      let filterEpisode = episodeList.filter(
+        (element) =>
+          element.name.toLowerCase().includes(term) ||
+          element.summary.toLowerCase().includes(term)
+      );
+      //This part displays the searched elements
+      displayPage(filterEpisode);
+      episodeNum.textContent = `${filterEpisode.length} episode(s)`;
+    });
+
+    allShow.forEach((show) => {
+      const option = `<option value=${show.id}>${show.name}</option>`;
+      selectShow.innerHTML += option;
+    });
+
+    selectShow.addEventListener("change", (e) => {
+      const showId = e.target.value;
+      fetch(`https://api.tvmaze.com/shows/${showId}/episodes`)
+        .then((response) => response.json())
+        .then((data) => {
+          displayPage(data);
+          oneShow(data);
+          // displayEpisode(data);
+        })
+        .catch((error) => console.log(error));
+    });
+
+    function oneShow(episode) {
+      episode.forEach((e) => {
+        let option = document.createElement("option");
+        option.value = e.id;
+        option.innerHTML = `S${e.season < 10 ? 0 : ""}${e.season} E${
+          e.number < 10 ? 0 : ""
+        }${e.number}-${e.name}`;
+        selectEpisode.appendChild(option);
+        selectEpisode.addEventListener("change", (event) => {
+          let newEpisode = event.target.value;
+          let filteredEpisode = episode.filter((key) => {
+            if (key.id == newEpisode) {
+              return key;
+            }
+          });
+          displayPage(filteredEpisode);
+        });
+      });
+    }
+
+    // function displayEpisode(list) {
+
+    // }
+
+    function displayPage(episodeList) {
+      const container = document.getElementById("container");
+      container.innerHTML = "";
+      episodeList.forEach((element) => {
+        const episode = document.createElement("div");
+        const title = document.createElement("h3");
+        const img = document.createElement("img");
+        const summary = document.createElement("p");
+        episode.className = "episode";
+        title.className = "title";
+        img.className = "image";
+        summary.className = "summary";
+        title.innerHTML = `${element.name} - S${element.season < 10 ? 0 : ""}${
+          element.season
+        }E${element.number < 10 ? 0 : ""}${element.number}`;
+        episode.appendChild(title);
+        img.src = `${element.image.medium ? element.image.medium : null}`;
+        episode.appendChild(img);
+        summary.innerHTML = element.summary;
+        episode.appendChild(summary);
+        container.appendChild(episode);
+      });
+    }
+
+    homebtn.addEventListener("click", function () {
+      location.reload(true);
     });
   }
-
-  episodeList.forEach(e=> {
-    let option = document.createElement('option')
-    option.innerHTML = `S${e.season < 10 ? 0 : ""}${e.season} E${e.number < 10 ? 0 : ""}${e.number}-${e.name}`
-    option.value = episodeList[e]
-    select.appendChild(option)
-    option.addEventListener('select', ()=> 
-      displayPage(option)
-    )
-  })
-
-  homebtn.addEventListener('click', function() {
-    location.reload(true);
-  })
 }
 window.onload = setup;
